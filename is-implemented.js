650c583fa3c8e0ab59787919d6a10784a2e3648f
Mariusz Nowak <mariusz@medikoo.com>
2014-10-10 16:21:32 +0200

    Pass only extendable implementation of WeakMap

--- is-implemented.js
+++ is-implemented.js
@@ -1,15 +1,39 @@
 'use strict';
 
+var d = require('d')
+
+  , create = Object.create, setPrototypeOf = Object.setPrototypeOf;
+
 module.exports = function () {
-	var map;
+	var weakMap, SubWeakMap, x;
 	if (typeof WeakMap !== 'function') return false;
 	if (String(WeakMap.prototype) !== '[object WeakMap]') return false;
-	map = new WeakMap();
-	if (typeof map.set !== 'function') return false;
-	if (map.set({}, 1) !== map) return false;
-	if (typeof map.clear !== 'function') return false;
-	if (typeof map.delete !== 'function') return false;
-	if (typeof map.has !== 'function') return false;
+	try {
+		// WebKit doesn't support arguments and crashes
+		weakMap = new WeakMap([[x = {}, 'one'], [{}, 'two'], [{}, 'three']]);
+	} catch (e) {
+		return false;
+	}
+	if (typeof weakMap.set !== 'function') return false;
+	if (weakMap.set({}, 1) !== weakMap) return false;
+	if (typeof weakMap.clear !== 'function') return false;
+	if (typeof weakMap.delete !== 'function') return false;
+	if (typeof weakMap.has !== 'function') return false;
+	if (weakMap.get(x) !== 'one') return false;
 
+	// Non new call
+	try { weakMap = WeakMap(); } catch (e) { return false; } //jslint: ignore
+	if (!weakMap) return false;
+
+	// Extendable
+	if (setPrototypeOf) {
+		SubWeakMap = function () { WeakMap.apply(this, arguments); };
+		setPrototypeOf(SubWeakMap, WeakMap);
+		SubWeakMap.prototype = create(WeakMap.prototype, { constructor: d(SubWeakMap) });
+		try { weakMap = new SubWeakMap([[x, 'foo']]); } catch (e) { return false; }
+		try {
+			if (weakMap.get(x) !== 'foo') return false;
+		} catch (e) { return false; }
+	}
 	return true;
 };
